600975499438187a9a28e7ae22cab6ed41ddfad1
Minna Iholin <minna.iholin@welho.com>
2025-11-15 17:05:36 +0200

    Ask user for degree and phrase length

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ import trie
 """Explanation + Args + Returns + Attributes"""
 
 def main():
-    print("Luodaanpas uusia sloganeita!")
-    text = "Istuin puun alla ja mietiskelin. Moi, sanoi naapuri. Moi moi, sanoin minä."
-    slogan = trie.train_markov_chain(text)
-    #slogan = generate_slogan_with_word("moi")
-    print(slogan)
+    print("Luodaanpas uusia lauseita! Mitä astetta haluat käyttää?")
+    degree = input() #tarkista että validi
+    print("Entä monenko sanan pituisia lauseita?")
+    length = input() #tarkista että validi
+    print("Luodaan siis " + length + "sanan pituisia lauseita asteella " + degree)
+    text = "Istuin puun alla ja mietiskelin. Moi, ?? ##00sanoi fsf08098sas8 naapuri. Moi moi, sanoin minä."
+    #slogan = trie.train_markov_chain(text, degree)
+    #phrases = generate_phrases(length)
+    #print(slogan)
 
-def generate_random_slogan():
-    pass
-
-def generate_slogan_with_word(word):
-    slogan = trie.starts_with(word)
+def generate_phrases(length):
+    slogan = trie.starts_with(length)
     return slogan
     
 if __name__ == "__main__":
