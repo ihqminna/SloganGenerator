e83dfe827e0ea2da34f361c5ad809564c2f17b45
Minna Iholin <minna.iholin@welho.com>
2025-11-20 12:17:27 +0200

    Check that inputs are valid ints

--- app.py
+++ app.py
@@ -3,15 +3,30 @@ import trie
 """Explanation + Args + Returns + Attributes"""
 
 def main():
-    print("Luodaanpas uusia lauseita! Mitä astetta haluat käyttää?")
-    degree_str = input() #tarkista että validi
-    degree = int(degree_str)
-    print("Entä monenko sanan pituisia lauseita?")
-    length_str = input() #tarkista että validi
-    length = int(length_str)
-    print("Luodaan siis " + length_str + " sanan pituisia lauseita asteella " + degree_str)
-    trie.train_markov_chain(length, degree)
+    length, degree = ask_for_parameters()
+    print("Luodaan siis " + str(length) + " sanan pituisia lauseita asteella " + str(degree))
+    trained_trie = trie.train_markov_chain(degree)
+    for i in range(20):
+        sentence = trie.Trie.get_sentence(trained_trie, length, degree)
+        print(sentence)
     #phrases = generate_phrases(length)
+
+def ask_for_parameters():
+    degree = ask_for_value("Luodaanpas uusia lauseita! Mitä astetta haluat käyttää?")
+    length = ask_for_value("Entä monenko sanan pituisia lauseita?")
+    return degree, length
+
+def ask_for_value(question):
+    valid = False
+    print(question)
+    value = input()
+    valid = value.isnumeric()
+    while not valid:
+        print("Syötäthän arvon kokonaislukuna")
+        print(question)
+        value = input()
+        valid = value.isnumeric()
+    return int(value)
 
 def generate_phrases(length):
     pass
