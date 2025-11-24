a5b8e452e6b3d40d8b2f60a753a0d81e0e00a5b3
Minna Iholin <minna.iholin@welho.com>
2025-11-24 14:02:53 +0200

    Trie constructor test

--- src/tests/trie_test.py
+++ src/tests/trie_test.py
@@ -5,7 +5,13 @@ class TestTrie(unittest.TestCase):
     def setUp(self):
         print("Set up goes here")
 
-    def test_create_trie(self):
+    def test_create_empty_trie(self):
         test_trie = src.trie.Trie()
         sentence = src.trie.Trie.get_sentence(test_trie, 0, 0)
         self.assertEqual(sentence, [])
+
+    def test_create_trie_with_nodes(self):
+        test_trie = src.trie.Trie()
+        sentence = ["testaaminen", "on", "ihan", "kivaa"]
+        src.trie.Trie.insert_sentence(test_trie, sentence)
+        self.assertEqual(src.trie.Trie.get_sentence(test_trie, 4, 3), sentence)
