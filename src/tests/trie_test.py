86dee8db2ced75c5f09ad05809373786b8a3b80b
Minna Iholin <minna.iholin@welho.com>
2025-11-24 13:58:10 +0200

    Trie constructor test

--- src/tests/trie_test.py
+++ src/tests/trie_test.py
@@ -1,2 +1,11 @@
 import unittest
 import src.trie
+
+class TestTrie(unittest.TestCase):
+    def setUp(self):
+        print("Set up goes here")
+
+    def test_create_trie(self):
+        test_trie = src.trie.Trie()
+        sentence = src.trie.Trie.get_sentence(test_trie, 0, 0)
+        self.assertEqual(sentence, [])
